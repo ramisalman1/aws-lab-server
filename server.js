1a68a6d7fcc980eab21696ce27fe97cf9a72fccd
Rami Salman <33587571+ramisalman1@users.noreply.github.com>
2025-11-15 16:26:32 +0300

    adding migration

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "localhost";
 
 // Middleware
 app.use(express.json());
@@ -28,7 +29,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: "Internal server error" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
 });
