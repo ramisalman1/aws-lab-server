f50d7a1e1a6643e7981fce85e5464a50a56b2de7
Rami Salman <33587571+ramisalman1@users.noreply.github.com>
2025-11-15 15:45:46 +0300

    first code

---
+++ src/database/db.ts
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const pgp = require("pg-promise")();
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = pgp({
+  connectionString: connectionString,
+});
+
+const getTodoDb = async (): Promise<{ id: string; newitem: string }[]> => {
+  try {
+    const todos = await pool.query("SELECT * from todo");
+    return todos;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const addTodoDb = async (
+  newItem
+): Promise<{ id: string; newitem: string }[]> => {
+  try {
+    const todo = await pool.query(
+      `INSERT INTO todo (newItem) VALUES ($1) RETURNING id, newItem`,
+      [newItem]
+    );
+    return todo;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const clearTodoDb = async (req, res): Promise<void> => {
+  try {
+    await pool.query(`DELETE FROM todo`);
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = {
+  getTodoDb,
+  addTodoDb,
+  clearTodoDb,
+};
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: true, credential: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/todo", require("./routes/Todo"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+const port = process.env.PORT || 6000;
+const server = app.listen(port, () => {
+  console.log(`server started on: localhost:${port}`);
+});
+
+process.on("unhandledRejection", (err: any) => {
+  console.log(err.message);
+  server.close(() => process.exit(1));
+});
