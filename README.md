4f36cedd433d1abffc2973923d33d5cae807884f
Rami Salman <33587571+ramisalman1@users.noreply.github.com>
2025-11-15 16:17:27 +0300

    delete all

--- README.md
+++
@@ -1,1 +0,0 @@
-# aws-lab-server
--- src/routes/Todo.ts
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const todoController = require('../controller/Todo')
-
-router.get('/', todoController.getTodo)
-router.post('/', todoController.addTodo) 
-router.delete('/', todoController.clearTodo) 
-
-module.exports = router 
